6585df7980a0c09a38a6ed6f14ea2430b449c818
seita1996 <taha.seita@gmail.com>
2022-08-11 13:40:19 +0900

    Reading Firestore data with Cloud Functions

--- app/functions/src/index.ts
+++ app/functions/src/index.ts
@@ -1,9 +1,30 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+admin.initializeApp(functions.config().firebase);
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+// /helloWorld
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
+});
+
+// /getRecords
+export const getRecords = functions.https.onRequest((request, response) => {
+  const ref = admin.firestore().collection("memo");
+  ref.get().then((snapshot) => {
+    response.json(snapshot.docs);
+  }).catch((error) => {
+    response.status(500).send(error);
+  });
+});
+
+// /getRecord?docid=qwer
+export const getRecord = functions.https.onRequest((request, response) => {
+  const docid = `${request.query.docid}`;
+  const ref = admin.firestore().collection("memo").doc(docid);
+  ref.get().then((snapshot) => {
+    response.json(snapshot);
+  }).catch((error) => {
+    response.status(500).send(error);
+  });
 });
